de038214c8f6e0801b86bfe4df9d4103a2852021
Isha <dayal.isha6@gmail.com>
2018-01-07 13:00:51 +0530

    Binary Searcb and MergeSort

--- BinarySearchClient.java
+++ BinarySearchClient.java
@@ -1,31 +1,79 @@
 package JavaLearn;
 
+import java.util.Random;
+
 public class BinarySearchClient {
-	public static void main(String[] args){
-		String stringToBeSearched = "isha";
-		String[] str = {"ankit","bob","claire","david","isha"};
-		
-		BinarySearch binarysearch = new BinarySearch();
-	    int resultIndex1 = binarysearch.binarySearchforString(str,0,str.length-1,stringToBeSearched);
-	    System.out.println("the name "+stringToBeSearched+" is at "+resultIndex1+" index of array");	
+	public static void main(String[] args) {
+		MyBinarySearch binarysearch = new MyBinarySearch();
+		binarysearch.binarySearchCallerForInt();
+		binarysearch.binarySearchCallerForString();
+			
 	}
 }
 
-class BinarySearch{
- public int binarySearchforString(String[] str, int l, int r, String s) {
+ class MyBinarySearch{
+	 
+	 public void binarySearchCallerForString() {
+		 String stringToBeSearched = "bob";
+			String[] str = {"ankit","bob","claire","david","isha"};
+		    int resultIndex1 = binarySearchforString(str,0,str.length-1,stringToBeSearched);
+		    System.out.println("the name "+stringToBeSearched+" is at "+resultIndex1+" index of array");
+		 
+	 }
+	 
+	 public void binarySearchCallerForInt() {
+		 int[] arr = new int[10];
+			int[] arr1 = {10,20,30,40,50,60,70,80,90,100};
+		 Random random = new Random();
+			for(int i = 0; i<10;i++) {
+				arr[i] = random.nextInt(50);
+			}
+			int elementToBeSearched = 30;
+			Sorting sorting = new Sorting();
+			sorting.selectionSort(arr);
+			sorting.printArray(arr);
+			
+			int resultIndex = binarySearchForInt(arr, 0, 9, elementToBeSearched);
+			System.out.println("the data is at "+resultIndex+"th index of array");
+		 
+	 }
+	 /**
+	  * method to search element in array
+	  * @param arr array in which search will be done
+	  * @param l lower index of the array
+	  * @param r last index of the array
+	  * @param x the element to be searched
+	  * @return the index at which the element is present, if not present then return -1.
+	  */
+	 public int binarySearchForInt(int[] arr, int l, int r, int x) {
+		 if(r>=l) {
+		 int mid;
+		 mid = l + (r-l)/2;
+		 if(arr[mid]==x) {
+			 return mid;
+		 } 
+		  if( arr[mid]<x) {
+			return binarySearchForInt(arr,mid+1,r,x);
+		 } else {
+			return binarySearchForInt(arr,l,mid-1,x);
+		 }
+		 
+	 } return -1;
+	 }
+	
+	 public int binarySearchforString(String[] str, int l, int r, String s) {
 		 if(r>=l) {
 			 int mid = l + (r-l)/2;
-			System.out.println(mid);
+			
 			 if(s.compareToIgnoreCase(str[mid])==0) {
 				 return mid;
 			 }
 			 if (s.compareToIgnoreCase(str[mid])<0) {
-				return binarySearchforString(str,l,mid-1,s); //why return?
+				return binarySearchforString(str,l,mid-1,s);
 			 } else {
 				 return binarySearchforString(str,mid+1,r,s);
 			 }
 		 }
-		 
-		 return -1;
+		 	 return -1;
 	 }
 }
--- MergeSortClient.java
+++ MergeSortClient.java
@@ -1,13 +1,110 @@
 package JavaLearn;
 
 public class MergeSortClient {
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		System.out.println("Merge sort client");
+		int[] arrToBeSorted = { 2, 4, 7, 10, 1, 3, 9};//, 16, 15, 5, 8 };
+		Sorter sorter = new Sorter();
+		sorter.mergeSort(arrToBeSorted);
+		for(int elem:arrToBeSorted) {
+			System.out.print(elem+" ");
+		}
 	}
 }
 
 class Sorter {
-	public void mergeSort() {
-		
+	private void merge(int[] arr, int[] aux, int l, int mid, int r) {
+		int i = l;
+		int j = mid + 1;
+		int k = 0;
+
+		while (i <= mid && j <= r) {
+			if (arr[i] < arr[j]) {
+				System.out.println("Adding " + arr[i]);
+				aux[k++] = arr[i++];
+			} else {
+				System.out.println("Adding " + arr[j]);
+				aux[k++] = arr[j++];
+			}
+		}
+		while (i <= mid) {
+			System.out.println("Adding " + arr[i]);
+			aux[k++] = arr[i++];
+		}
+		while (j <= r) {
+			System.out.println("Adding " + arr[j]);
+			aux[k++] = arr[j++];
+		}
+		// copy the aux array into original array
+		System.out.println("Copying aux into arr");
+		for (i = l, k = 0; i <= r; i++) {
+			System.out.println("Storing in arr at "+i+" value "+aux[k]);
+			arr[i] = aux[k++];
+		}
+	}
+
+	private void mergeSortInternal(int[] arr, int[] aux, int l, int r) {
+		if (r <= l)
+			return;
+		int mid = l + (r - l) / 2;
+		System.out.println("l " + l + " m " + mid + " r " + r);
+		mergeSortInternal(arr, aux, l, mid);
+		mergeSortInternal(arr, aux, mid + 1, r);
+		merge(arr, aux, l, mid, r);
+	}
+
+	public void mergeSort(int[] arr) {
+		int[] aux = new int[arr.length];
+		mergeSortInternal(arr, aux, 0, arr.length - 1);
+
+	}
+
+	/**
+	 * Alternate logic to merge two arrays
+	 * 
+	 * @param arr1
+	 *            first array (sorted)
+	 * @param arr2
+	 *            second array (sorted)
+	 */
+	public void altMerge(int[] arr1, int[] arr2) {
+		int combinedLength = arr1.length + arr2.length;
+		int[] c = new int[combinedLength];
+		int a1 = 0; // counter for arr1
+		int a2 = 0; // counter for arr2
+		for (int k = 0; k < combinedLength; k++) {
+			if (a1 >= arr1.length) {
+				// arr1 exhausted
+				// copy the elements of arr2
+				System.out.println("Arr1 Exhausted");
+				System.out.println("Adding " + arr2[a2]);
+				c[k] = arr2[a2];
+				a2++;
+			} else if (a2 >= arr2.length) {
+				// arr2 exhausted
+				// copy the elements of arr1
+				System.out.println("Arr2 Exhausted");
+				System.out.println("Adding " + arr1[a1]);
+				c[k] = arr1[a1];
+				a1++;
+			} else {
+				if (arr1[a1] < arr2[a2]) {
+					// put the smaller
+					System.out.println("Arr1 is smaller");
+					System.out.println("Adding " + arr1[a1]);
+					c[k] = arr1[a1];
+					a1++;
+				} else {
+					System.out.println("Arr2 is smaller");
+					System.out.println("Adding " + arr2[a2]);
+					c[k] = arr2[a2];
+					a2++;
+				}
+			}
+		}
+		System.out.println("Merged array");
+		for (int x : c) {
+			System.out.print(x + " ");
+		}
 	}
 }
