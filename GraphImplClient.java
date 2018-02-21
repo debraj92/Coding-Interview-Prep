05c5ec3e9061e97ccbde147a748da7d0cf452208
Debraj Ray <debrajray@Debrajs-MacBook-Air.local>
2018-02-21 23:12:38 +0530

    DFS traversal of a graph

--- GraphImplClient.java
+++ GraphImplClient.java
@@ -3,15 +3,63 @@ package JavaLearn;
 import java.util.LinkedList;
 
 public class GraphImplClient {
+	
+	/**
+	 *  Graph 1:
+	 *  		0 -> 1 -> 2  <-    5 -> 6
+	 *      |    |    |   |    |    ^
+	 *      V    |    V   |    V    |
+	 *      4    -->  3   |    7 -> 8
+	 *      |_____________|
+	 *      
+	 *      Two disconnected components
+	 *      Total number of vertices = 9
+	 *      
+	 *  Graph 2 (https://www.geeksforgeeks.org/depth-first-traversal-for-a-graph/):    
+	 *  
+	 *    0 --> 1
+	 *   ^ |    |
+	 *   | V    |
+	 *    2 <----
+	 *    |
+	 *    ------>3->
+	 *           ^ |
+	 *           | V
+	 *           <--
+	 */ 
+	
+	
 	public static void main(String[]args) {
-		MyGraph mygraph = new MyGraph(5);
+		
+		//Graph 1
+		/*
+		MyGraph mygraph = new MyGraph(9);
 		mygraph.addEdge(0, 1);
 		mygraph.addEdge(0, 4);
 		mygraph.addEdge(1, 2);
 		mygraph.addEdge(1, 3);
 		mygraph.addEdge(2, 3);
 		mygraph.addEdge(4, 2);
+		mygraph.addEdge(5, 6);
+		mygraph.addEdge(5, 7);
+		mygraph.addEdge(7, 8);
+		mygraph.addEdge(8, 6);
+		*/
+		
+		// Graph 2
+		MyGraph mygraph = new MyGraph(4);
+		mygraph.addEdge(0, 2);
+		mygraph.addEdge(2, 0);
+		mygraph.addEdge(0, 1);
+		mygraph.addEdge(1, 2);
+		mygraph.addEdge(2, 3);
+		mygraph.addEdge(3, 3);
+		
 		mygraph.printAdjList();
+		System.out.println("**** DFS TRAVERSAL OF ENTIRE GRAPH ****");
+		mygraph.dfsUtil();
+		System.out.println("**** DFS TRAVERSAL FROM VERTEX 2 ****");
+		mygraph.dfsFromVertex(2);
 	}
 }
 
@@ -59,5 +107,55 @@ class MyGraph {
 			}
 			System.out.println();
 		}
+	}
+	
+	boolean[] visited;
+	
+	public void dfsUtil() {
+		// visited array should be initialized for every dfs traversal call
+		visited = new boolean[countOfVertices];
+		// this loop is needed to print all the disconnected components
+		for(int i= 0; i < countOfVertices; i++) {
+			dfs(i);
+		}
+	}
+	
+	/**
+	 * DFS traversal is similar to preorder traversal of a binary tree
+	 */
+	private void dfs (int v) {
+		if (visited[v]) {
+			return;
+		}
+		System.out.println(v);
+		visited[v] = true;
+		for(int i= 0; i < countOfVertices; i++) {
+			if(isEdgePresent(v, i)) {
+				dfs(i);
+			}
+		}
+	}
+	
+	/**
+	 *  This method is only for test. It starts DFS from a specific vertex and only traverse the component
+	 *  of which the vertex is a part. We should actually use the dfsUtil method for traversing the entire graph
+	 * @param start
+	 */
+	public void dfsFromVertex (int start) {
+		visited = new boolean[countOfVertices];
+		dfs(start);
+	}
+	
+	/**
+	 * Checks if an edge is present from src to dst vertex
+	 * @param src
+	 * @param dst
+	 * @return
+	 */
+	private boolean isEdgePresent (int src, int dst) {
+		if (adjList[src].contains(dst)) {
+			return true;
+		}
+		return false;
 	}
 }
